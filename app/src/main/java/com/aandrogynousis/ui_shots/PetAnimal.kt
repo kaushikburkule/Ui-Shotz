53c137ba5979a37c4de342d29105c890421e9c06
Kaushik Burkule <kaushikburkule@gmail.com>
2020-08-24 17:37:58 +0530

    PetAnimal

---
+++ app/src/main/java/com/aandrogynousis/ui_shots/PetAnimal.kt
@@ -0,0 +1,11 @@
+package com.aandrogynousis.ui_shots
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PetAnimal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pet_animal)
+    }
+}
