a735b4c793cfcb67847a274ea722a5969e5a7b75
Kaushik Burkule <kaushikburkule@gmail.com>
2020-08-24 21:59:20 +0530

    Previous Layouts

---
+++ app/src/main/java/com/aandrogynousis/ui_shots/MusicPlayer.kt
@@ -0,0 +1,11 @@
+package com.aandrogynousis.ui_shots
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MusicPlayer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_player)
+    }
+}
---
+++ app/src/main/java/com/aandrogynousis/ui_shots/WalletPayment.kt
@@ -0,0 +1,11 @@
+package com.aandrogynousis.ui_shots
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WalletPayment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallet_payment)
+    }
+}
